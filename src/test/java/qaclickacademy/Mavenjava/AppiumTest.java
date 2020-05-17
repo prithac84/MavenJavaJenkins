36e53da786f6b0581970b4bfcd7ce9e988adbcd6
prithac84 <prithac84@gmail.com>
2020-05-18 03:09:50 +0530

    First time uploading mavenjava to git repository

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid() {
+		
+		System.out.println("Inside Appium BrowserAutomation");
+		
+	}
+	@Test
+	public void IOSApps() {
+		System.out.println("Inside Appium IOSApps()");
+		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("Inside RESTAPITest postJira");
+		
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("Inside RESTAPITest deleteTwitter");
+		
+	}
+
+}
